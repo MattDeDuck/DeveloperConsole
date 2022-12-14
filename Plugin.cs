8dc09192f95cc2622326f8a257951565d34a59df
MattDeDuck <Minihobbsch@gmail.com>
2022-12-14 15:52:51 +0000

    Initial upload

---
+++ Plugin.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using PotionCraft.ManagersSystem.Debug;
+using PotionCraft.ManagersSystem.Game;
+using PotionCraft.SceneLoader;
+
+
+namespace DeveloperConsole
+{
+    [BepInPlugin(PLUGIN_GUID, "PotionCraftDevConsole", "1.0.2.1")]
+    [BepInProcess("Potion Craft.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string PLUGIN_GUID = "com.mattdeduck.potioncraftdevconsole";
+
+        public static ManualLogSource Log { get; private set; }
+
+        private void Awake()
+        {
+            Log = Logger;
+            Log.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+            Log.LogInfo($"Plugin {PLUGIN_GUID}: Patch Succeeded!");
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(GameManager), "Start")]
+        public static void Start_Postfix()
+        {
+            ObjectsLoader.AddLast("SaveLoadManager.SaveNewGameState", () => EnableDevConsole());
+        }
+
+        public static void EnableDevConsole()
+        {
+            DebugManager.isDeveloperMode = true;
+            Log.LogInfo("Dev Console is enabled");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# Developer Console
+
+Developer Console is a mod for Potion Craft: Alchemist Simulator that enables the debug console.
+
+This mod is designed to work with the 1.0.2.1 version of Potion Craft
+
+### BepInEx
+- Download the latest BepInEx 5 release from [here](https://github.com/BepInEx/BepInEx/releases)
+Note: You need the `BepInEx_64` version for use with Potion Craft
+- You can read the installation for BepInEx [here](https://docs.bepinex.dev/articles/user_guide/installation/index.html)
+- Once installed run once to generate the config files and folders
+
+#### Developer Console installation
+- Download the latest version from the [releases page](https://github.com/MattDeDuck/DeveloperConsole/releases)
+- Unzip the folder
+- Copy the folder containing the `DeveloperConsole.dll` file into `Potion Craft/BepInEx/plugins/`
+- Run the game
+
+### How to Use
+Simply hit the `F4` button on your keyboard to toggle the debug console button in the top left of your screen
